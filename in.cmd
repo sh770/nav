b7e456836ebeb44c719c6fb1ac264fc118674d1c
Shalom Harpaz <sh770h@gmail.com>
2025-10-22 23:31:54 +0300

    remove

--- in.cmd
+++ in.cmd
@@ -1,1 +1,1 @@
-"index.html","style.css","script.js" | ForEach-Object { New-Item -Name $_ -ItemType File }
+New-Item -Name "project" -ItemType Directory | Out-Null; "index.html","style.css","script.js" | ForEach-Object { New-Item -Path "project\$_" -ItemType File }
